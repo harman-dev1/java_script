8e942fd0993e907075091c9a34a82f20f9051644
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-25 21:52:28 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log("2" > 1); // in these kind of comparison result is not always predictable
+
+// console.log(null > 0)
+// console.log(null == 0) // equality check and comparison both are different things in js
+// console.log(null >= 0) // js convert null to 0 if we use '>' or '<'
+
+// console.log(undefined >= 0) // for undefined you'll always get ans false
+
+console.log("2" == 2) // true
+console.log("2" === 2) // triple check also checks data type if data type not matched then it won't let to convert data types
