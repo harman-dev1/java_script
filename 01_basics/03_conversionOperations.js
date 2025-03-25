c5da44cc8f2233f7150580115fabae775aa25c95
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-25 18:19:55 +0000

    Data Types Conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "undefined";
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+/*Notes
+// "33" => 33
+// "33abc" => NaN; datatype(number)
+// true => 1 ;false => 0
+*/
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+/*Notes
+//1 => true
+//0 => false
+//"" => false
+//"harman" => true
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
