2a383f820ed6515d37d464a3ab9cf04cfd0659cd
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-25 17:31:25 +0000

    dataTypes basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // it will treat whole js code as new version
+// alert("hello"); // we are using nodeJS not browser
+
+let name = "Harman"
+let age = 19
+
+
+//Data Types
+/* Primitive Data types
+//number :2 => 53
+//bigint
+//string
+//boolean
+//null : it is data type of 'object' and also a standalone value 
+//undefined :value is not assigned yet
+//symbol
+*/
+//object
+
+console.table([typeof(age), typeof(name), typeof(null), typeof undefined])
