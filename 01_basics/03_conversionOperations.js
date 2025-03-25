5d5499c2125a8fb9c19cb177d403e96433a08316
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-25 21:38:35 +0000

    done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,5 +27,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33;
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***********Operations**************//
+let val = 3;
+let negVal = -val;
+//console.log(negVal)
+
+let str1 = "har"
+let str2 = "man"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(typeof str3)
+
+// console.log(1 + "2");// no conversion only side by side add
+// console.log("1" + 2);// no conversion only side by side add
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2") // if from left first one is number then it consider as nymber and then on second number we'll add
+
+// console.log(+true) // it will become 1 of +ve
+// console.log(-true) // it will become 1 of -ve
+// console.log( +"") // 0 ans
+
