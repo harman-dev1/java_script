33078a6b664582fdb1d93eaa58450d83898e5dc9
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-26 10:16:44 +0000

    summary Of Data Types + Heap and Stack memory concept

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -8,7 +8,7 @@ let name;
 const symbol = Symbol('123')
 
 const bigNumber = 112736786546765456n
-console.log(typeof bigNumber); // bigInt data type
+// console.log(typeof bigNumber); // bigInt data type
 
 //Reference (Non Primitive) //these have datatypes of object 
 //Arrays, Objects, Functions
@@ -22,3 +22,23 @@ const fn = function () { // data type is a object function
 }
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//+++++++++++++++++++++++
+
+//stack (Primitive), Heap (Non-Primitive)
+
+let myNickName = "Harman";
+let anotherName = myNickName;
+anotherName = "Hussain"
+console.log(anotherName);
+console.log(myNickName);
+
+let userOne = {
+    email :"harmanrick81@gmail.com",
+    name :"Harman"
+}
+
+let userTwo = userOne;
+userTwo.email = "hussain@gmail.com";
+console.table([userOne, userTwo]);
