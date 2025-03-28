ef3d25e3a18a779d0a5f86fd8b0eae440ead92a7
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-28 16:07:59 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if(3 != "3") { // 3 == "3" so it return false for cyrrent if
+//     console.log("equal");
+    
+// }
+
+// if(3 !== "3") { // 3 is not equal to "3" if we use strict check 
+//     console.log("not equal"); 
+// }
+
+
+// const balance = 1000;
+// if (balance < 5000) console.log('test1'), console.log('test2'); // we can use multiple lines usin comma instead of curly braces but it's a inappropirate way
+
