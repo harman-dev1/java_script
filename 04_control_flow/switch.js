071b0dffab9073ff1c81972689f4730da527e91f
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-28 16:16:55 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,51 @@
+//***********SYNTAX */
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+
+// switch (month) { //shift + alt + down to copy and paste code
+//     case 1:
+//         console.log('January');
+//         break;
+//     case 2:
+//         console.log('FEB');
+//         break;
+//     case 3:
+//         console.log('MARCH');
+//         break; // if we don't use break then all below code will run 
+//     case 4:
+//         console.log('APRIL');
+//         break;
+//     case 5:
+//         console.log('MAY');
+//         break;
+
+//     default:
+//         console.log("Default case");
+        
+//         break;
+// }
+
+const month  = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("Januray");
+        break;
+    case "feb":
+        console.log("FEBRURAY");
+        break;
+    case "mar":
+        console.log("Januray");
+        break;
+
+    default:
+        break;
+}
