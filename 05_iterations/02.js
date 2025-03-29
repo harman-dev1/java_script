c3c8c2acbff3adee3c92692a34ee3a2096af1279
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2025-03-29 09:25:33 +0000

    while & do while loop

---
+++ 05_iterations/02.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+let myArr = ["Superman", "Batman", "Flash"] 
+let arr = 0
+while (arr < myArr.length) {
+    console.log(myArr[arr]);
+    arr++;
+}
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score = score + 2;
+} while (score <10);
